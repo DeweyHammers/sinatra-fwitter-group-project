b4640d24a0d9b78ec356a7430536bc8478a7c6b6
Dewey <hammersdrichard@gmail.com>
2021-03-08 20:43:44 -0800

    Done.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,3 @@
 class Tweet < ActiveRecord::Base
-  include Slugs::InstanceMethods
-  extend Slugs::ClassMethods
-
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
-  include Slugs::InstanceMethods
-  extend Slugs::ClassMethods
-
   has_secure_password
   has_many :tweets
+
+  def slug
+    self.username.downcase.gsub(/\s/, '-')
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find{ |instance| instance.slug == slug }
+  end
 end
